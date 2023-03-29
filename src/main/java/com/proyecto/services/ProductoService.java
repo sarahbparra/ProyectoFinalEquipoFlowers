aaaa2c0abfcbb8c1720ea4c0d939762e5d089860
sarah <sxrahhp@gmail.com>
2023-03-29 22:31:00 +0200

    Eliminado método duplicado

--- src/main/java/com/proyecto/services/ProductoService.java
+++ src/main/java/com/proyecto/services/ProductoService.java
@@ -3,6 +3,7 @@ package com.proyecto.services;
 import java.util.List;
 
 import com.proyecto.entities.Comprador;
+import com.proyecto.entities.Pedido;
 import com.proyecto.entities.Producto;
 import com.proyecto.entities.Proveedor;
 
@@ -16,5 +17,4 @@ public interface ProductoService {
     public List<Producto> findByProveedor(Proveedor proveedor);
     public List<Producto> findByComprador(Comprador comprador);
     public List<Producto> findByPedido (Pedido pedido);
-    public List<Producto> findByComprador (Comprador comprador);
 }
