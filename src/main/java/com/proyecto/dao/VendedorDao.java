9535e13b1d841fd5dcf30cdb9d26db6b1c47c9bd
zineb <zinebaf96@gmail.com>
2023-03-29 20:37:15 +0200

    Capa Vendedor

---
+++ src/main/java/com/proyecto/dao/VendedorDao.java
@@ -0,0 +1,12 @@
+package com.proyecto.dao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.proyecto.entities.Vendedor;
+
+
+
+@Repository
+
+public interface VendedorDao extends JpaRepository <Vendedor, Integer> { 
+    
+}
