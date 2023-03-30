68d9fff20c30fea4dd21009d695986dbace7f223
IreneG <irene.gonza.mor@gmail.com>
2023-03-30 19:58:17 +0200

    AdminService creado

---
+++ src/main/java/com/proyecto/services/AdministradorService.java
@@ -0,0 +1,27 @@
+package com.proyecto.services;
+
+import java.util.List;
+
+import com.proyecto.entities.Administrador;
+
+public class AdministradorService {
+
+    public List<Administrador> findAll();
+
+    public Administrador findById(int idAdministrador);
+
+    public void save(Administrador administrador);
+
+    public void deleteById(int idAdministrador);
+
+    public void delete(Administrador administrador);
+
+    /**
+     * No es necesario un metodo update, porque el save inserta o actualiza, en
+     * dependencia
+     * de que el idAdministrador exista o no, es decir, si no existe lo crea, y si
+     * existe actualiza
+     * la informacion.
+     */
+
+}
