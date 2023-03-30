49c8e0b861ac781c26e37dde819e203ea2387b8d
IreneG <irene.gonza.mor@gmail.com>
2023-03-30 20:46:54 +0200

    Admin entinty, dao y service comentados en javaDoc

--- src/main/java/com/proyecto/dao/AdministradorDao.java
+++ src/main/java/com/proyecto/dao/AdministradorDao.java
@@ -5,7 +5,29 @@ import org.springframework.stereotype.Repository;
 
 import com.proyecto.entities.Administrador;
 
+/**
+ * 
+ * Esta interfaz define la capa DAO para la entidad Administrador.
+ * Utiliza Spring Data JPA para proporcionar métodos CRUD básicos para la
+ * entidad.
+ */
 @Repository
 public interface AdministradorDao extends JpaRepository<Administrador, Integer> {
-
+    
 }
+/**
+ * La anotación @Repository indica que esta interfaz es un componente de la capa
+ * de acceso a datos y permite que Spring gestione la creación de instancias de
+ * esta clase.
+ * 
+ * La interfaz extiende la interfaz JpaRepository, que proporciona métodos
+ * básicos para acceder a la base de datos relacionales utilizando JPA (Java
+ * Persistence API). La entidad administrador es la entidad gestionada por esta
+ * capa y Integer es el tipo de datos del identificador de la entidad
+ * Administrador en la base de datos.
+ * 
+ * No se proporciona ninguna implementación explícita para los métodos CRUD, ya
+ * que Spring Data JPA se encarga de crear la implementación en tiempo de
+ * ejecución en función de los nombres de los métodos definidos en esta
+ * interfaz.
+ */
