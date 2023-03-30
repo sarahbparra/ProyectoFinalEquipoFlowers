f900492619f8680bc61dc7c2fd926e5eff52345a
IreneG <irene.gonza.mor@gmail.com>
2023-03-30 22:38:49 +0200

    Segundo Admin agregado a App y error corregido

--- src/main/java/com/proyecto/controllers/MainController.java
+++ src/main/java/com/proyecto/controllers/MainController.java
@@ -1,11 +1,15 @@
 package com.proyecto.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.proyecto.services.AdministradorService;
 
 @Controller
 @RequestMapping("/")
 
 public class MainController {
     
+    /**Métodos correspondientes a Administrador: */
 }
