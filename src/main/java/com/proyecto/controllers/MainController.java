6c0c2ae7ea69c6a68f2ff28a5dd256c0affa2ee1
celia <celia.cava18ruiz@gmail.com>
2023-03-31 08:43:02 +0200

    Main Controller de listar producto

--- src/main/java/com/proyecto/controllers/MainController.java
+++ src/main/java/com/proyecto/controllers/MainController.java
@@ -1,11 +1,63 @@
 package com.proyecto.controllers;
 
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.proyecto.entities.Comprador;
+import com.proyecto.entities.Producto;
+import com.proyecto.entities.Proveedor;
+import com.proyecto.services.CompradorService;
+import com.proyecto.services.ProductoService;
 
 @Controller
 @RequestMapping("/")
 
 public class MainController {
+    
+    private static final Logger LOG = Logger.getLogger("MainController");
+
+    @Autowired
+    private ProductoService productoService;
+
+
+/**Método que lista productos */
+
+@GetMapping("/listarP")
+public ModelAndView listarProducto() {
+
+    List<Producto> productos = productoService.findAll();
+
+    ModelAndView mav = new ModelAndView("views/listarProductos");
+    mav.addObject("productos", productos);
+
+    return mav;
+}
+
+// /**Método de alta productos */
+
+// @GetMapping("/AltaProducto")
+// public String AltaProducto(Model model) {
+
+//     List<Proveedor> proveedores = proveedorService.findAll();
+    
+//     Producto producto = new Producto();
+
+//     model.addAttribute("producto", producto);
+//     model.addAttribute("proveedores", proveedores);
+
+//     return "views/formularioAltaProducto";
+    
+// }
+
+
+
     
 }
